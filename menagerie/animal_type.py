a79773af9d13a8a9b2264b21eac1be51a80e1d03
ivangorcanuk <ivangorcanuk464@gmail.com>
2022-12-13 00:23:31 +0300

    First commit

---
+++ menagerie/animal_type.py
@@ -0,0 +1,14 @@
+class Predator:  # хищные
+    pass
+
+class NonPredator:  # нехищные
+    pass
+
+class Ground:  # наземные
+    pass
+
+class Underwater:  # подводные
+    pass
+
+class Winged:  # крылатые
+    pass
