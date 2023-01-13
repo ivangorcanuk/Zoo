4bdd1716fe550a85a92d649e67ce7184d4a72e2d
ivangorcanuk <ivangorcanuk464@gmail.com>
2023-01-13 14:57:07 +0300

    Finished with the logic in the rating of animals window. I registered the methods with buttons as static and combined them in the main window class.

--- menagerie/GUI.py
+++ menagerie/GUI.py
@@ -1,5 +1,5 @@
 import tkinter as tk
-from main import AnimalRatings, SeeAnimals
+from main import Data
 
 class MainMenu:  # главное меню
     def __init__(self):
@@ -8,13 +8,30 @@ class MainMenu:  # главное меню
         self.main.geometry(f'240x271+100+200')
         self.main.title('Zoo')
 
-        self.button('Add an animal', self.open_window1).grid(row=1, column=0, sticky='wens', padx=50, pady=2)
-        self.button('Animal Ratings', self.open_window2).grid(row=2, column=0, sticky='wens', padx=50, pady=2)
-        self.button('See Animals', self.open_window3).grid(row=3, column=0, sticky='wens', padx=50, pady=2)
-        self.button('Exid', self.main.destroy).grid(row=4, column=0, sticky='wens', padx=50, pady=2)
+        self.button(self.main, 'Add an animal', self.open_window1).grid(row=1, column=0, sticky='wens', padx=50, pady=2)
+        self.button(self.main, 'Animal Ratings', self.open_window2).grid(row=2, column=0, sticky='wens', padx=50, pady=2)
+        self.button(self.main, 'See Animals', self.open_window3).grid(row=3, column=0, sticky='wens', padx=50, pady=2)
+        self.button(self.main, 'Exid', self.main.destroy).grid(row=4, column=0, sticky='wens', padx=50, pady=2)
 
-    def button(self, text, command):
-        return tk.Button(self.main, text=text, font=('Arial', 13), command=command)
+    @staticmethod
+    def button(window, text, command):
+        return tk.Button(window, text=text, font=('Arial', 13), command=command)
+
+    @staticmethod
+    def label(window, text):
+        return tk.Label(window, text=text, font=('Arial', 13))
+
+    @staticmethod
+    def entry(window, variable):
+        return tk.Entry(window, font=('Arial', 10), textvariable=variable)
+
+    @staticmethod
+    def radiobutton(window, value, variable):
+        return tk.Radiobutton(window, text=value, font=('Arial', 13), variable=variable, value=value)
+
+    @staticmethod
+    def text(window):
+        return tk.Text(window, font=('Arial', 13), bg='#33ffe6')
 
     def open_window1(self):
         addingAnimals = AddingAnimals()
@@ -25,7 +42,7 @@ class MainMenu:  # главное меню
     def open_window3(self):
         deleteAnimal = SeeAnimals()
 
-############################################################################################################################################
+"""Добавление животного"""
 
 class AddingAnimals:  # добавить животное
     def __init__(self):
@@ -43,55 +60,43 @@ class AddingAnimals:  # добавить животное
         self.value_weight = tk.StringVar()  # создали переменную для веса животного
         self.value_nickname = tk.StringVar()  # создали переменную для клички животного
 
-        self.label('Выберите подвид животного:').grid(row=0, columnspan=3, )
-        self.radiobutton('ground', self.valueStrSubspecies).grid(row=1, column=0)
-        self.radiobutton('underwater', self.valueStrSubspecies).grid(row=1, column=1)
-        self.radiobutton('winged', self.valueStrSubspecies).grid(row=1, column=2)
+        MainMenu.label(self.addingAnimals, 'Выберите подвид животного:').grid(row=0, columnspan=3)
+        MainMenu.radiobutton(self.addingAnimals, 'ground', self.valueStrSubspecies).grid(row=1, column=0)
+        MainMenu.radiobutton(self.addingAnimals, 'underwater', self.valueStrSubspecies).grid(row=1, column=1)
+        MainMenu.radiobutton(self.addingAnimals, 'winged', self.valueStrSubspecies).grid(row=1, column=2)
 
-        self.label('Укажите среду обитания данного подвида:').grid(row=2, columnspan=3)
-        self.radiobutton('forests', self.valueStrHabitat).grid(row=3, column=0)
-        self.radiobutton('mountains', self.valueStrHabitat).grid(row=4, column=0)
-        self.radiobutton('steppes', self.valueStrHabitat).grid(row=3, column=1)
-        self.radiobutton('rivers', self.valueStrHabitat).grid(row=4, column=1)
-        self.radiobutton('seas', self.valueStrHabitat).grid(row=3, column=2)
-        self.radiobutton('ocean', self.valueStrHabitat).grid(row=4, column=2)
+        MainMenu.label(self.addingAnimals, 'Укажите среду обитания данного подвида:').grid(row=2, columnspan=3)
+        MainMenu.radiobutton(self.addingAnimals, 'forests', self.valueStrHabitat).grid(row=3, column=0)
+        MainMenu.radiobutton(self.addingAnimals, 'mountains', self.valueStrHabitat).grid(row=4, column=0)
+        MainMenu.radiobutton(self.addingAnimals, 'steppes', self.valueStrHabitat).grid(row=3, column=1)
+        MainMenu.radiobutton(self.addingAnimals, 'rivers', self.valueStrHabitat).grid(row=4, column=1)
+        MainMenu.radiobutton(self.addingAnimals, 'seas', self.valueStrHabitat).grid(row=3, column=2)
+        MainMenu.radiobutton(self.addingAnimals, 'ocean', self.valueStrHabitat).grid(row=4, column=2)
 
-        self.label('Являются ли миграционным:').grid(row=5, columnspan=3)
-        self.radiobutton('yes', self.valueBoolIsMigratory).grid(row=6, column=0)
-        self.radiobutton('no', self.valueBoolIsMigratory).grid(row=6, column=1)
+        MainMenu.label(self.addingAnimals, 'Являются ли миграционным:').grid(row=5, columnspan=3)
+        MainMenu.radiobutton(self.addingAnimals, 'yes', self.valueBoolIsMigratory).grid(row=6, column=0)
+        MainMenu.radiobutton(self.addingAnimals, 'no', self.valueBoolIsMigratory).grid(row=6, column=1)
 
-        self.label('Укажите климатические условия:').grid(row=7, columnspan=3)
-        self.radiobutton('warm', self.valueStrClimate).grid(row=8, column=0)
-        self.radiobutton('cold', self.valueStrClimate).grid(row=8, column=1)
-        self.radiobutton('moderate', self.valueStrClimate).grid(row=8, column=2)
+        MainMenu.label(self.addingAnimals, 'Укажите климатические условия:').grid(row=7, columnspan=3)
+        MainMenu.radiobutton(self.addingAnimals, 'warm', self.valueStrClimate).grid(row=8, column=0)
+        MainMenu.radiobutton(self.addingAnimals, 'cold', self.valueStrClimate).grid(row=8, column=1)
+        MainMenu.radiobutton(self.addingAnimals, 'moderate', self.valueStrClimate).grid(row=8, column=2)
 
-        self.label('Явлиется ли зверь хищником:').grid(row=9, columnspan=3)
-        self.radiobutton('yes', self.valueBoolIsPredator).grid(row=10, column=0)
-        self.radiobutton('no', self.valueBoolIsPredator).grid(row=10, column=1)
+        MainMenu.label(self.addingAnimals, 'Явлиется ли зверь хищником:').grid(row=9, columnspan=3)
+        MainMenu.radiobutton(self.addingAnimals, 'yes', self.valueBoolIsPredator).grid(row=10, column=0)
+        MainMenu.radiobutton(self.addingAnimals, 'no', self.valueBoolIsPredator).grid(row=10, column=1)
 
-        self.label('Какую пищу употребляет:').grid(row=11, column=0)
-        self.entry(self.value_food).grid(row=11, column=1)
+        MainMenu.label(self.addingAnimals, 'Какую пищу употребляет:').grid(row=11, column=0)
+        MainMenu.entry(self.addingAnimals, self.value_food).grid(row=11, column=1)
 
-        self.label('Укажите вес животного:').grid(row=12, column=0)
-        self.entry(self.value_weight).grid(row=12, column=1)
+        MainMenu.label(self.addingAnimals, 'Укажите вес животного:').grid(row=12, column=0)
+        MainMenu.entry(self.addingAnimals, self.value_weight).grid(row=12, column=1)
 
-        self.label('Придумайте кличку \n для нового жителя зоопарка:').grid(row=13, column=0)
-        self.entry(self.value_nickname).grid(row=13, column=1)
+        MainMenu.label(self.addingAnimals, 'Придумайте кличку \n для нового жителя зоопарка:').grid(row=13, column=0)
+        MainMenu.entry(self.addingAnimals, self.value_nickname).grid(row=13, column=1)
 
-        self.button('exit', self.addingAnimals.destroy).grid(row=17, column=0)
-        self.button('save', self.save).grid(row=17, column=2)
-
-    def radiobutton(self, value, variable):
-        return tk.Radiobutton(self.addingAnimals, text=value, font=('Arial', 13), variable=variable, value=value)
-
-    def label(self, text):
-        return tk.Label(self.addingAnimals, text=text, font=('Arial', 13))
-
-    def entry(self, variable):
-        return tk.Entry(self.addingAnimals, font=('Arial', 10), textvariable=variable)
-
-    def button(self, text, command):
-        return tk.Button(self.addingAnimals, text=text, font=('Arial', 13), command=command)
+        MainMenu.button(self.addingAnimals, 'exit', self.addingAnimals.destroy).grid(row=17, column=0)
+        MainMenu.button(self.addingAnimals, 'save', self.save).grid(row=17, column=2)
 
     def save(self):
         subspecies_value = self.valueStrSubspecies.get()
@@ -103,74 +108,127 @@ class AddingAnimals:  # добавить животное
         weight_value = self.value_weight.get()
         nickname_value = self.value_nickname.get()
         print(subspecies_value, habitat_value, climate_value, migratory_boolean, predator_boolean, food_value, weight_value, nickname_value)
+        self.addingAnimals.destroy()
 
-##################################################################################################################################
+"""Просмотр рейтингов"""
 
-# class SeeAnimals:  # просмотр животных
-#     def __init__(self):
-#         self.seeAnimals = tk.Toplevel()
-#         self.seeAnimals.grab_set()
-#         self.seeAnimals['bg'] = '#33ffe6'
-#         self.seeAnimals.geometry(f'550x300+500+50')
-#         self.seeAnimals.title('See animals')
-#
-#         self.button('Top 3 Lightest Zoo Creatures', self.open_window2_1).grid(row=1, padx=30, pady=2)
-#         self.button('Top 5 biggest predators', self.open_window2_2).grid(row=2, padx=30, pady=2)
-#         self.button('List of herbivore names', self.open_window2_3).grid(row=3, padx=30, pady=2)
-#         self.button('List of underwater creatures in decreasing order of their weight', self.open_window2_4).grid(row=4, padx=30, pady=2)
-#         self.button('List of land animals, with each name and location', self.open_window2_5).grid(row=5, padx=30, pady=2)
-#         self.button('Exit', self.seeAnimals.destroy).grid(row=6, padx=30, pady=2)
-#
-#     def button(self, text, command):
-#         return tk.Button(self.seeAnimals, text=text, font=('Arial', 13), command=command)
-#
-#     def label(self, text):
-#         return tk.Label(self.seeAnimals, text=text, font=('Arial', 13))
-#
-#     def text(self):
-#         return tk.Text(self.seeAnimals, font=('Arial', 13), bg='blue')
-#
-#     def open_window(self):
-#         self.label('топ 3 самых легких существа зоопарка').pack(anchor='n')
-#         self.text().place(x=60, y=60, width=300, height=100)
-#         WorkingMethods().view_lungs_animal(self.text, Data().listAnimal)  # вызывали метод из файла WorkingMethods
-#         tk.Button(self.window2_1, width=20, text="1. Exid", font=('Arial', 13), command=self.window2_1.destroy).pack(
-#             expand=True)
-#
-#     def open_window2_1(self):
-#         button2_1 = Button2_1()
-#
-#     def open_window2_2(self):
-#         button2_2 = Button2_2()
-#
-#     def open_window2_3(self):
-#         button2_3 = Button2_3()
-#
-#     def open_window2_4(self):
-#         button2_4 = Button2_4()
-#
-#     def open_window2_5(self):
-#         button2_5 = Button2_5()
+class AnimalRatings:  # просмотр животных
+    def __init__(self):
+        self.animalRatings = tk.Toplevel()
+        self.animalRatings.grab_set()
+        self.animalRatings['bg'] = '#33ffe6'
+        self.animalRatings.geometry(f'550x500+500+50')
+        self.animalRatings.title('Animal Ratings')
 
-##########################################################################################################################################
+        MainMenu.button(self.animalRatings, 'Top 3 Lightest Zoo Creatures', lambda: self.open_window(1)).grid(row=1, column=0, padx=30, pady=2)
+        MainMenu.button(self.animalRatings, 'Top 5 biggest predators', lambda: self.open_window(2)).grid(row=2, column=0, padx=30, pady=2)
+        MainMenu.button(self.animalRatings, 'List of herbivore names', lambda: self.open_window(3)).grid(row=3, column=0, padx=30, pady=2)
+        MainMenu.button(self.animalRatings, 'List of underwater creatures in decreasing order of their weight', lambda: self.open_window(4)).grid(row=4, column=0, padx=30, pady=2)
+        MainMenu.button(self.animalRatings, 'List of land animals, with each name and location', lambda: self.open_window(5)).grid(row=5, column=0, padx=30, pady=2)
+        MainMenu.button(self.animalRatings, 'Exit', self.animalRatings.destroy).grid(row=6, column=0, padx=30, pady=2)
+        self.text = MainMenu.text(self.animalRatings)
+        self.text.insert('end', f'Выберите текс')
+        self.text.place(x=120, y=270, width=300, height=100)
 
-# class DeleteAnimal:  # удалить животных
-#     def __init__(self):
-#         self.deleteAnimal = tk.Toplevel()
-#         self.deleteAnimal.grab_set()
-#         self.deleteAnimal['bg'] = '#33ffe6'
-#         self.deleteAnimal.geometry(f'550x500+500+50')
-#         self.deleteAnimal.title('DeleteAnimal')
-#         self.animal = tk.StringVar()  # создали переменную для выбора животного
-#
-#         tk.Label(self.deleteAnimal, text='Enter the name of the animal', font=('Arial', 13)).pack()
-#         tk.Entry(self.deleteAnimal, font=('Arial', 10), textvariable=self.animal).pack()
-#         tk.Button(self.deleteAnimal, text='exit', font=('Arial', 13), command=self.deleteAnimal.destroy)
-#         self.text = tk.Text(self.deleteAnimal, font=('Arial', 13), bg='#33ffe6')
-#         self.text.place(x=120, y=270, width=300, height=100)
+    def open_window(self, ark):
+        self.text.delete('1.0', 'end')  # удалили предыдущий текст в текстовом окне
+        if ark == 1:
+            list_animal = Data().listAnimal
+            for i in range(len(list_animal)):  # топ 3 самых легких существа зоопарка
+                for j in range(i, len(list_animal)):
+                    if list_animal[i] > list_animal[j]:
+                        f = list_animal[j]
+                        list_animal[j] = list_animal[i]
+                        list_animal[i] = f
+                stroka = list_animal[i].nickname + ' ' + list_animal[i].clasAnimal + ' ' + str(list_animal[i].weight) + ' кг'
+                self.text.insert('end', f'{stroka}\n')  # выводим строку
+                if i == 2:
+                    break
+        elif ark == 2:
+            list_predator = Data().listPredator
+            for i in range(len(list_predator)):  # топ 5 самых больших хищников
+                for j in range(i, len(list_predator)):
+                    if list_predator[i] < list_predator[j]:
+                        f = list_predator[j]
+                        list_predator[j] = list_predator[i]
+                        list_predator[i] = f
+                stroka = list_predator[i].nickname + ' ' + list_predator[i].clasAnimal + ' ' + str(list_predator[i].weight) + ' кг'
+                self.text.insert('end', f'{stroka}\n')
+                if i == 4:
+                    break
+        elif ark == 3:
+            list_herbivorous = Data().listHerbivorous
+            for herbivorous in list_herbivorous:  # просмотр кличек травоядных существ
+                stroka = herbivorous.nickname + ' ' + herbivorous.clasAnimal
+                self.text.insert('end', f'{stroka}\n')
+        elif ark == 4:
+            list_underwater = Data().listUnderwater
+            for i in range(len(list_underwater)):  # просмотр подводных существ по мере убывания их веса
+                for j in range(i, len(list_underwater)):
+                    if list_underwater[i] < list_underwater[j]:
+                        f = list_underwater[j]
+                        list_underwater[j] = list_underwater[i]
+                        list_underwater[i] = f
+                stroka = list_underwater[i].nickname + ' ' + list_underwater[i].clasAnimal + ' ' + str(list_underwater[i].weight) + ' кг'
+                self.text.insert('end', f'{stroka}\n')
+        elif ark == 5:
+            list_ground = Data().listGround
+            for ground in list_ground:  # просмотр наземных животных с кличкой каждого и местом обитания
+                stroka = ground.nickname + ' ' + ground.clasAnimal + ' ' + ground.weight + ' кг'
+                self.text.insert('end', f'{stroka}\n')
 
+"""Просмотр животных"""
 
-#########################################################################################################################################3
+class SeeAnimals:  # просмотр
+    def __init__(self):
+        self.seeAnimals = tk.Toplevel()
+        self.seeAnimals.grab_set()
+        self.seeAnimals['bg'] = '#33ffe6'
+        self.seeAnimals.geometry(f'550x500+500+50')
+        self.seeAnimals.title('See Animals')
+        self.choice_animal = tk.StringVar()  # создали переменную для выбора животного
+
+        MainMenu.label(self.seeAnimals, text='Enter the name of the animal:').grid(row=0, column=0)
+        MainMenu.entry(self.seeAnimals, self.choice_animal).grid(row=0, column=1)
+        MainMenu.label(self.seeAnimals, 'В зоопарке нет животного с данным именем').grid(row=1, column=0)
+        MainMenu.button(self.seeAnimals, 'exit', self.seeAnimals.destroy).grid(row=2, column=1)
+        MainMenu.button(self.seeAnimals, 'search', self.selected_animal).grid(row=2, column=0)
+        self.text = MainMenu.text(self.seeAnimals)
+        self.brings_out_animals()
+        self.text.place(x=120, y=100, width=300, height=300)
+
+    def brings_out_animals(self):
+        for an in Data().listAnimal:
+            stroka = an.nickname + ' ' + an.clasAnimal
+            self.text.insert('end', f'{stroka}\n')
+
+    def selected_animal(self):
+        print('ter')
+        name_animal = self.choice_animal.get()
+        if self.examination(name_animal):  # вызвали функицю класс-метод
+            window_animal = tk.Toplevel()
+            window_animal.grab_set()
+            window_animal.geometry(f'550x500+500+50')
+            window_animal.title('Animal Information')
+            MainMenu.button(window_animal, 'exit', window_animal.destroy).grid(row=1, column=1)
+            text = MainMenu.text(window_animal)
+            text.place(x=120, y=270, width=300, height=100)
+            WorkingMethods().view_all_animals(text, Data().listAnimal)
+        else:
+            print('xzfg')
+
+    def examination(self, animalName):  # проверяем есть ли выбранное животное в общем списке
+        for name in Data().listAnimal:
+            if name.nickname == animalName:
+                return True
+        return False
+
+    def view_all_animals(self, name_animal):  # проверяем есть ли выбранное животное в общем списке
+        for name in Data().listAnimal:
+            if name.nickname == name_animal:
+                return True
+        return False
+
 
 mainMenu = MainMenu().main
 mainMenu.mainloop()
