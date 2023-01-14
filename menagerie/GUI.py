249e3f60a82878f94938d1cfe8c9636b9d904e79
ivangorcanuk <ivangorcanuk464@gmail.com>
2023-01-14 17:27:21 +0300

    Added inheritance from tkinter classes.

--- menagerie/GUI.py
+++ menagerie/GUI.py
@@ -1,17 +1,17 @@
 import tkinter as tk
 from main import Data
 
-class MainMenu:  # главное меню
+class MainMenu(tk.Tk):  # главное меню
     def __init__(self):
-        self.main = tk.Tk()
-        self.main['bg'] = '#33ffe6'
-        self.main.geometry(f'430x380+500+50')
-        self.main.title('Zoo')
+        super().__init__()
+        self['bg'] = '#33ffe6'
+        self.geometry(f'430x380+500+50')
+        self.title('Zoo')
 
-        self.button(self.main, 'Add an animal', self.open_window1).place(x=60, y=30, width=300, height=20)
-        self.button(self.main, 'Animal Ratings', self.open_window2).place(x=60, y=60, width=300, height=20)
-        self.button(self.main, 'See Animals', self.open_window3).place(x=60, y=90, width=300, height=20)
-        self.button(self.main, 'Exit', self.main.destroy).place(x=60, y=150, width=300, height=20)
+        self.button(self, 'Add an animal', self.open_window1).place(x=60, y=30, width=300, height=20)
+        self.button(self, 'Animal Ratings', self.open_window2).place(x=60, y=60, width=300, height=20)
+        self.button(self, 'See Animals', self.open_window3).place(x=60, y=90, width=300, height=20)
+        self.button(self, 'Exit', self.destroy).place(x=60, y=150, width=300, height=20)
 
     @staticmethod
     def button(window, text, command):
@@ -34,69 +34,71 @@ class MainMenu:  # главное меню
         return tk.Text(window, font=('Arial', 13), bg='#33ffe6')
 
     def open_window1(self):
-        addingAnimals = AddingAnimals()
+        addingAnimals = AddingAnimals(self)
+        addingAnimals.grab_set()
 
     def open_window2(self):
-        seeAnimals = AnimalRatings()
+        seeAnimals = AnimalRatings(self)
+        seeAnimals.grab_set()
 
     def open_window3(self):
-        deleteAnimal = SeeAnimals()
+        deleteAnimal = SeeAnimals(self)
+        deleteAnimal.grab_set()
 
 """Добавление животного"""
 
-class AddingAnimals:  # добавить животное
-    def __init__(self):
-        self.addingAnimals = tk.Toplevel()
-        self.addingAnimals.grab_set()
-        self.addingAnimals.title('Adding animals')
-        self.addingAnimals['bg'] = '#33ffe6'
-        self.addingAnimals.geometry(f'460x670+500+50')
-        self.valueStrSubspecies = tk.StringVar(self.addingAnimals, 'esth')  # создали переменную со строковым значением подвид животного, которое будет возвращать наша радиокнопка
-        self.valueStrHabitat = tk.StringVar(self.addingAnimals, 'est')  # аналогично для среды обитания
-        self.valueStrClimate = tk.StringVar(self.addingAnimals, 'es')  # для климатических условий
-        self.valueBoolIsMigratory = tk.StringVar(self.addingAnimals, '1')  # создали переменную с числовым значением - миграция
-        self.valueBoolIsPredator = tk.StringVar(self.addingAnimals, '2')  # создали переменную с числовым значением - хищник
+class AddingAnimals(tk.Toplevel):  # добавить животное
+    def __init__(self, parent):
+        super().__init__(parent)
+        self.title('Adding animals')
+        self['bg'] = '#33ffe6'
+        self.geometry(f'460x670+500+50')
+        self.valueStrSubspecies = tk.StringVar(self, 'esth')  # создали переменную со строковым значением подвид животного, которое будет возвращать наша радиокнопка
+        self.valueStrHabitat = tk.StringVar(self, 'est')  # аналогично для среды обитания
+        self.valueStrClimate = tk.StringVar(self, 'es')  # для климатических условий
+        self.valueBoolIsMigratory = tk.StringVar(self, '1')  # создали переменную с числовым значением - миграция
+        self.valueBoolIsPredator = tk.StringVar(self, '2')  # создали переменную с числовым значением - хищник
         self.value_food = tk.StringVar()  # создали переменную для пищи животного
         self.value_weight = tk.StringVar()  # создали переменную для веса животного
         self.value_nickname = tk.StringVar()  # создали переменную для клички животного
 
-        MainMenu.label(self.addingAnimals, 'Выберите подвид животного:').place(x=10, y=10, width=240, height=20)
-        MainMenu.radiobutton(self.addingAnimals, 'ground', self.valueStrSubspecies).place(x=10, y=40, width=80, height=20)
-        MainMenu.radiobutton(self.addingAnimals, 'underwater', self.valueStrSubspecies).place(x=10, y=60, width=110, height=20)
-        MainMenu.radiobutton(self.addingAnimals, 'winged', self.valueStrSubspecies).place(x=10, y=80, width=80, height=20)
+        MainMenu.label(self, 'Выберите подвид животного:').place(x=10, y=10, width=240, height=20)
+        MainMenu.radiobutton(self, 'ground', self.valueStrSubspecies).place(x=10, y=40, width=80, height=20)
+        MainMenu.radiobutton(self, 'underwater', self.valueStrSubspecies).place(x=10, y=60, width=110, height=20)
+        MainMenu.radiobutton(self, 'winged', self.valueStrSubspecies).place(x=10, y=80, width=80, height=20)
 
-        MainMenu.label(self.addingAnimals, 'Укажите среду обитания данного подвида:').place(x=10, y=110, width=340, height=20)
-        MainMenu.radiobutton(self.addingAnimals, 'forests', self.valueStrHabitat).place(x=10, y=140, width=80, height=20)
-        MainMenu.radiobutton(self.addingAnimals, 'mountains', self.valueStrHabitat).place(x=10, y=160, width=100, height=20)
-        MainMenu.radiobutton(self.addingAnimals, 'steppes', self.valueStrHabitat).place(x=10, y=180, width=85, height=20)
-        MainMenu.radiobutton(self.addingAnimals, 'rivers', self.valueStrHabitat).place(x=10, y=200, width=65, height=20)
-        MainMenu.radiobutton(self.addingAnimals, 'seas', self.valueStrHabitat).place(x=10, y=220, width=60, height=20)
-        MainMenu.radiobutton(self.addingAnimals, 'ocean', self.valueStrHabitat).place(x=10, y=240, width=70, height=20)
+        MainMenu.label(self, 'Укажите среду обитания данного подвида:').place(x=10, y=110, width=340, height=20)
+        MainMenu.radiobutton(self, 'forests', self.valueStrHabitat).place(x=10, y=140, width=80, height=20)
+        MainMenu.radiobutton(self, 'mountains', self.valueStrHabitat).place(x=10, y=160, width=100, height=20)
+        MainMenu.radiobutton(self, 'steppes', self.valueStrHabitat).place(x=10, y=180, width=85, height=20)
+        MainMenu.radiobutton(self, 'rivers', self.valueStrHabitat).place(x=10, y=200, width=65, height=20)
+        MainMenu.radiobutton(self, 'seas', self.valueStrHabitat).place(x=10, y=220, width=60, height=20)
+        MainMenu.radiobutton(self, 'ocean', self.valueStrHabitat).place(x=10, y=240, width=70, height=20)
 
-        MainMenu.label(self.addingAnimals, 'Являются ли миграционным:').place(x=10, y=270, width=240, height=20)
-        MainMenu.radiobutton(self.addingAnimals, 'yes', self.valueBoolIsMigratory).place(x=10, y=300, width=55, height=20)
-        MainMenu.radiobutton(self.addingAnimals, 'no', self.valueBoolIsMigratory).place(x=10, y=320, width=45, height=20)
+        MainMenu.label(self, 'Являются ли миграционным:').place(x=10, y=270, width=240, height=20)
+        MainMenu.radiobutton(self, 'yes', self.valueBoolIsMigratory).place(x=10, y=300, width=55, height=20)
+        MainMenu.radiobutton(self, 'no', self.valueBoolIsMigratory).place(x=10, y=320, width=45, height=20)
 
-        MainMenu.label(self.addingAnimals, 'Укажите климатические условия:').place(x=10, y=350, width=270, height=20)
-        MainMenu.radiobutton(self.addingAnimals, 'warm', self.valueStrClimate).place(x=10, y=380, width=70, height=20)
-        MainMenu.radiobutton(self.addingAnimals, 'cold', self.valueStrClimate).place(x=10, y=400, width=60, height=20)
-        MainMenu.radiobutton(self.addingAnimals, 'moderate', self.valueStrClimate).place(x=10, y=420, width=95, height=20)
+        MainMenu.label(self, 'Укажите климатические условия:').place(x=10, y=350, width=270, height=20)
+        MainMenu.radiobutton(self, 'warm', self.valueStrClimate).place(x=10, y=380, width=70, height=20)
+        MainMenu.radiobutton(self, 'cold', self.valueStrClimate).place(x=10, y=400, width=60, height=20)
+        MainMenu.radiobutton(self, 'moderate', self.valueStrClimate).place(x=10, y=420, width=95, height=20)
 
-        MainMenu.label(self.addingAnimals, 'Явлиется ли зверь хищником:').place(x=10, y=450, width=245, height=20)
-        MainMenu.radiobutton(self.addingAnimals, 'yes', self.valueBoolIsPredator).place(x=10, y=480, width=55, height=20)
-        MainMenu.radiobutton(self.addingAnimals, 'no', self.valueBoolIsPredator).place(x=10, y=500, width=45, height=20)
+        MainMenu.label(self, 'Явлиется ли зверь хищником:').place(x=10, y=450, width=245, height=20)
+        MainMenu.radiobutton(self, 'yes', self.valueBoolIsPredator).place(x=10, y=480, width=55, height=20)
+        MainMenu.radiobutton(self, 'no', self.valueBoolIsPredator).place(x=10, y=500, width=45, height=20)
 
-        MainMenu.label(self.addingAnimals, 'Какую пищу употребляет:').place(x=10, y=530, width=215, height=20)
-        MainMenu.entry(self.addingAnimals, self.value_food).place(x=230, y=530, width=220, height=20)
+        MainMenu.label(self, 'Какую пищу употребляет:').place(x=10, y=530, width=215, height=20)
+        MainMenu.entry(self, self.value_food).place(x=230, y=530, width=220, height=20)
 
-        MainMenu.label(self.addingAnimals, 'Укажите вес животного:').place(x=10, y=560, width=195, height=20)
-        MainMenu.entry(self.addingAnimals, self.value_weight).place(x=230, y=560, width=220, height=20)
+        MainMenu.label(self, 'Укажите вес животного:').place(x=10, y=560, width=195, height=20)
+        MainMenu.entry(self, self.value_weight).place(x=230, y=560, width=220, height=20)
 
-        MainMenu.label(self.addingAnimals, 'Придумайте кличку для\n нового жителя зоопарка:').place(x=10, y=590, width=195, height=40)
-        MainMenu.entry(self.addingAnimals, self.value_nickname).place(x=230, y=600, width=220, height=20)
+        MainMenu.label(self, 'Придумайте кличку для\n нового жителя зоопарка:').place(x=10, y=590, width=195, height=40)
+        MainMenu.entry(self, self.value_nickname).place(x=230, y=600, width=220, height=20)
 
-        MainMenu.button(self.addingAnimals, 'exit', self.addingAnimals.destroy).place(x=380, y=640, width=70, height=20)
-        MainMenu.button(self.addingAnimals, 'save', self.save).place(x=300, y=640, width=70, height=20)
+        MainMenu.button(self, 'exit', self.destroy).place(x=380, y=640, width=70, height=20)
+        MainMenu.button(self, 'save', self.save).place(x=300, y=640, width=70, height=20)
 
     def save(self):
         subspecies_value = self.valueStrSubspecies.get()
@@ -108,25 +110,24 @@ class AddingAnimals:  # добавить животное
         weight_value = self.value_weight.get()
         nickname_value = self.value_nickname.get()
         print(subspecies_value, habitat_value, climate_value, migratory_boolean, predator_boolean, food_value, weight_value, nickname_value)
-        self.addingAnimals.destroy()
+        self.destroy()
 
 """Просмотр рейтингов"""
 
-class AnimalRatings:  # просмотр животных
-    def __init__(self):
-        self.animalRatings = tk.Toplevel()
-        self.animalRatings.grab_set()
-        self.animalRatings['bg'] = '#33ffe6'
-        self.animalRatings.geometry(f'430x380+500+50')
-        self.animalRatings.title('Animal Ratings')
+class AnimalRatings(tk.Toplevel):  # просмотр животных
+    def __init__(self, parent):
+        super().__init__(parent)
+        self['bg'] = '#33ffe6'
+        self.geometry(f'430x380+500+50')
+        self.title('Animal Ratings')
 
-        MainMenu.button(self.animalRatings, 'Top 3 Lightest Zoo Creatures', lambda: self.open_window(1)).place(x=60, y=30, width=300, height=20)
-        MainMenu.button(self.animalRatings, 'Top 5 biggest predators', lambda: self.open_window(2)).place(x=60, y=60, width=300, height=20)
-        MainMenu.button(self.animalRatings, 'List of herbivore names', lambda: self.open_window(3)).place(x=60, y=90, width=300, height=20)
-        MainMenu.button(self.animalRatings, 'List of underwater creatures\n in decreasing order of their weight', lambda: self.open_window(4)).place(x=60, y=120, width=300, height=40)
-        MainMenu.button(self.animalRatings, 'List of land animals,\n with each name and location', lambda: self.open_window(5)).place(x=60, y=170, width=300, height=40)
-        MainMenu.button(self.animalRatings, 'Exit', self.animalRatings.destroy).place(x=60, y=350, width=300, height=20)
-        self.text = MainMenu.text(self.animalRatings)
+        MainMenu.button(self, 'Top 3 Lightest Zoo Creatures', lambda: self.open_window(1)).place(x=60, y=30, width=300, height=20)
+        MainMenu.button(self, 'Top 5 biggest predators', lambda: self.open_window(2)).place(x=60, y=60, width=300, height=20)
+        MainMenu.button(self, 'List of herbivore names', lambda: self.open_window(3)).place(x=60, y=90, width=300, height=20)
+        MainMenu.button(self, 'List of underwater creatures\n in decreasing order of their weight', lambda: self.open_window(4)).place(x=60, y=120, width=300, height=40)
+        MainMenu.button(self, 'List of land animals,\n with each name and location', lambda: self.open_window(5)).place(x=60, y=170, width=300, height=40)
+        MainMenu.button(self, 'Exit', self.destroy).place(x=60, y=350, width=300, height=20)
+        self.text = MainMenu.text(self)
         self.text.insert('end', f'Выберите текс')
         self.text.place(x=60, y=230, width=300, height=100)
 
@@ -179,22 +180,21 @@ class AnimalRatings:  # просмотр животных
 
 """Просмотр животных"""
 
-class SeeAnimals:  # просмотр
-    def __init__(self):
-        self.seeAnimals = tk.Toplevel()
-        self.seeAnimals.grab_set()
-        self.seeAnimals['bg'] = '#33ffe6'
-        self.seeAnimals.geometry(f'430x380+500+50')
-        self.seeAnimals.title('See Animals')
+class SeeAnimals(tk.Toplevel):  # просмотр
+    def __init__(self, parent):
+        super().__init__(parent)
+        self['bg'] = '#33ffe6'
+        self.geometry(f'430x380+500+50')
+        self.title('See Animals')
         self.choice_animal = tk.StringVar()  # создали переменную для выбора животного
 
-        MainMenu.label(self.seeAnimals, text='Enter the name of the animal:').place(x=10, y=10, width=220, height=20)
-        MainMenu.entry(self.seeAnimals, self.choice_animal).place(x=240, y=10, width=100, height=20)
-        self.label = MainMenu.label(self.seeAnimals, 'Список всех животных зоопарка')
+        MainMenu.label(self, text='Enter the name of the animal:').place(x=10, y=10, width=220, height=20)
+        MainMenu.entry(self, self.choice_animal).place(x=240, y=10, width=100, height=20)
+        self.label = MainMenu.label(self, 'Список всех животных зоопарка')
         self.label.place(x=90, y=40, width=285, height=20)
-        MainMenu.button(self.seeAnimals, 'exit', self.seeAnimals.destroy).place(x=350, y=350, width=70, height=20)
-        MainMenu.button(self.seeAnimals, 'search', self.selected_animal).place(x=350, y=10, width=70, height=20)
-        self.text = MainMenu.text(self.seeAnimals)
+        MainMenu.button(self, 'exit', self.destroy).place(x=350, y=350, width=70, height=20)
+        MainMenu.button(self, 'search', self.selected_animal).place(x=350, y=10, width=70, height=20)
+        self.text = MainMenu.text(self)
         self.brings_out_animals()
         self.text.place(x=70, y=70, width=300, height=270)
 
